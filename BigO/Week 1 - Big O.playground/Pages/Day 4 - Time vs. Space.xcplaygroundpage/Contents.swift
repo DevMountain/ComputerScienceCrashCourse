2fc920b114ecfd25a5694ac93341ed11326f150a
Trevor Walker <Walker123t@gmail.com>
2020-05-01 16:20:05 -0600

    Updated Space Complexity and Collection Types playground

--- BigO/Week 1 - Big O.playground/Pages/Day 4 - Time vs. Space.xcplaygroundpage/Contents.swift
+++ BigO/Week 1 - Big O.playground/Pages/Day 4 - Time vs. Space.xcplaygroundpage/Contents.swift
@@ -1,53 +1,52 @@
-// Big O Day 4 - Time vs. Space
+/**
+ Bool, Char, Int8 = 1
+ Int16, short = 2
+ floats, Int32, Int, long = 4
+ Double, Int64 = 8
+ Collection = (Size of type)n
+ */
 
 
-// What is the space complexity of the two following functions?
-func factorial(_ n: Int) -> Int {
-    if n < 0 {
-        return -1
-    } else if n == 0 {
-        return 1
-    } else {
-        return n * factorial(n - 1)
-    }
+// Time - O(1)
+// Space Real - O(12)
+// Space Simple - O(1)
+func ex1(a: Int, b: Int, c: Int) -> Int { //a - 4, b - 4, c - 4
+    return a + b + c
 }
 
-
-
-
-
-// Time Complexity vs. Space Complexity
-// What is the difference between time and space complexity?
-// How do we count time complexity? How many loops it does. How long it takes.
-// How do we count space complexity? How much storage are you using? How much memory are you using?
-// - The size of your arrays, or maps, or other data structures.
-// - Recursive calls add 1 element of space per call.
-
-
-
-func fib(_ n: Int) -> Int {
-    if n <= 0 {
-        return 0
-    } else if n == 1 {
-        return 1
-    } else {
-        return fib(n-1) + fib(n-2)
+// Time - O(n)
+// Space Real - 4n + 12
+// Space Simple - O(n)
+func ex2(a: [Int], b: Int) -> Int { // a - 4n, b - 4
+    var total: Int = 0 // total - 4
+    
+    for i in a { //i - 4
+        total += i
     }
+    
+    total += b
+    return total
+}
+// Time - O(n)
+// Space Real - O(n)
+// Space Simple - O(n)
+func ex3(name: String) -> String {
+    return name.lowercased()
 }
 
-
-
-
-
-var memoFibNumbers: [Int:Int] = [1:1, 2:1]
-func memoFibNumber(_ number: Int) -> Int {
-    if let fibNumber = memoFibNumbers[number] {
-        return fibNumber
-    } else {
-        memoFibNumbers[number] = memoFibNumber(number - 1) + memoFibNumber(number - 2)
-        return memoFibNumbers[number]!
-    }
+// Time - nlog(n)
+// Space Real - 4(4n) + 4
+// Space Simple - O(n)
+// Space with Stack - m log(n)
+// Simple Stack Space - log(n)
+func quicksort(_ a: [Int]) -> [Int] { // 4n
+    guard a.count > 1 else { return a }
+    
+    let pivot = a[a.count/2] // 4
+    let less = a.filter { $0 < pivot } // 4n
+    let equal = a.filter { $0 == pivot } // 4n
+    let greater = a.filter { $0 > pivot } // 4n
+    
+    return quicksort(less) + equal + quicksort(greater)
 }
-
-//: [Prev](@previous)
 
